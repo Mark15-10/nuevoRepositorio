6d33e43a67caf2dd9c43e1d8e119a759ab9980ff
Mario Rojas <mario.rojaset12d1@gmail.com>
2025-05-29 21:42:41 -0300

    objetos

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-var lista = [];
+class Silla{
+    color ;
+    tamaño;
+    peso;
+    patas;
+    estado;
 
-function ingresar(){
-    var numero = document.getElementById("numero-ingresado")
-    lista.push(parseInt(numero.value))
+    mover(){}
+
+    romper(){
+        return 'la silla' +' '+sillaRota.color + 'esta rota'
+    }
+    
+    arreglar(){}
+    
+    
+};
+
+var sillaRota = new Silla();
+var p = document.getElementById('resultado');
+sillaRota.color = 'azul '
+
+function mostrar(){
+    p.innerText = sillaRota.romper()
+
+    
+
 }
-
-function mostrar (){
-    var p1 = document.getElementById('p1')
-    var p2 = document.getElementById('p2')
-    var p3 = document.getElementById('p3')
-    var p4 = document.getElementById('p4')
-    
-    
-    p1.innerText ='EL valor mas alto ingresado es: '+ Math.max(...lista)
-    p2.innerText = 'El valor minimo ingresado es : '+Math.min(...lista)
-    
-    var sumaTotal = 0 ;
-    for(var i = 0 ; i<lista.length; i++){sumaTotal+=lista[i]};
-    p3.innerHTML = 'La suma de todos los valores ingresados es: ' + sumaTotal
-    p4.innerText = 'El promedio es : '+sumaTotal/lista.length
-}
-
-
-// max min promedio sumaTotal   
